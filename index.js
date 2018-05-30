64bf07577dde2737d5acc1b162081eca2199127e
j-ornelas <johnnyoh1988@gmail.com>
2018-05-29 17:36:31 -0700

    Adds AlbumList to main app

--- index.js
+++ index.js
@@ -5,11 +5,16 @@
 
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header.js';
+import AlbumList from './src/components/AlbumList.js';
 
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 };
 
