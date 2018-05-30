e1730e6527c69f15274813f34de71227d23b55cb
j-ornelas <johnnyoh1988@gmail.com>
2018-05-29 20:52:58 -0700

    Added http fetch request to lastFM api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetails from './AlbumDetails';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
 
   componentWillMount() {
     fetch('https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=jazz&api_key=b52d1dca839938408e89902843289ea8&format=json')
      .then(response => response.json())
      .then((responseData) => {
-       console.log(responseData.albums.album);
+       console.log('fetch data', responseData.albums.album);
+       console.log('state will mount', this.state.albums);
+       this.setState({ albums: responseData.albums.album });
+     })
+     .then(() => {
+       console.log('state will mount 2', this.state.albums);
      })
      .catch((error) => {
        console.log(error);
      });
    }
 
+   renderlist() {
+     return this.state.albums.map((album, index) => <AlbumDetails key={index} album={album} />);
+   }
+
   render() {
     return (
       <View>
-      <Text>here is the album list component</Text>
+        {this.renderlist()}
       </View>
     );
   }
