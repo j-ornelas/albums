95f19ce8a01c15a98f203735aa85b27d6af4d7c2
j-ornelas <johnnyoh1988@gmail.com>
2018-05-30 22:20:33 -0700

    Added images and text Views to Album Detail

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View, Linking } from 'react-native';
 import Card from './Card.js';
 import CardSection from './CardSection.js';
+import Button from './Button.js';
 
-const AlbumDetails = (props) => {
+const AlbumDetails = ({ album }) => {
+  const { name, artist, image } = album;
+  const { thumbnailStyle, headerContentStyle,
+          imageViewStyle, albumNameStyle,
+          artistNameStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.name}</Text>
+        <View style={imageViewStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: image[2]['#text'] }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={albumNameStyle}>{name}</Text>
+          <Text style={artistNameStyle}>{artist.name}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(image[2]['#text'])}>
+          Remove
+        </Button>
       </CardSection>
     </Card>
   );
 };
 
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+    width: null
+  },
+  thumbnailStyle: {
+    height: 100,
+    width: 100
+  },
+  imageViewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingRight: 10
+  },
+  albumNameStyle: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  artistNameStyle: {
+    fontSize: 18
+  }
+};
 
 export default AlbumDetails;
