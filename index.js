0a454494c2ce7288f705a7f94d3ade5377eaae86
j-ornelas <johnnyoh1988@gmail.com>
2018-06-02 16:05:35 -0700

    Removed all render logic from index.js

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-//
-// AppRegistry.registerComponent('albums', () => App);
-
-
-import React from 'react';
-import { View, AppRegistry } from 'react-native';
-import Header from './src/components/Header.js';
-import AlbumList from './src/components/AlbumList.js';
-
-const App = () => {
-  return (
-    <View>
-      <Header headerText={'Albums'} />
-      <AlbumList />
-    </View>
-  );
-};
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
 AppRegistry.registerComponent('albums', () => App);
